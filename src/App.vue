592792553b5afa07f417bc7acda3ccb4620ce044
MircoFiocchi <Mircofiocchi@gmail.com>
2019-04-16 14:20:54 -0300

    Login

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,13 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+  <div id='app'>
+    <div id='nav'>
+      <router-link to='/'></router-link>
+      <router-link to='/'></router-link>
+    </div>
+    <router-view></router-view>
+  </div>
 </template>
+
 
 <script>
 import HelloWorld from './components/HelloWorld'
---
+++ src/views/Inicio.vue
@@ -0,0 +1,53 @@
+<template>
+<v-flex>
+  <v-toolbar dark color="#006064">
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+
+    <v-toolbar-title class="white--text">SUPERLALI</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn icon>
+      <v-icon>search</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>person</v-icon>
+    </v-btn>
+
+    <template v-slot:extension>
+      <v-toolbar-title class="white--text">Inicio de Sesion </v-toolbar-title>
+    </template>
+
+  </v-toolbar>
+  <v-spacer></v-spacer>
+   <v-layout position:Center  justify-center>
+
+      <v-card ref="form">
+        <v-card-text row>
+          <v-text-field
+            ref="gmail"
+            v-model="name"
+            label="Email"
+            placeholder="pepito@gmail.com"
+            required
+          ></v-text-field>
+          <v-text-field
+            ref="contrasena"
+            v-model="contrasena"
+            label="Contrasena"
+            placeholder="******"
+            counter="25"
+            required
+          ></v-text-field>
+
+        </v-card-text>
+        <v-btn block color="#006064" >Ingresar </v-btn>
+      
+        <v-btn block color="#4DB6AC" >Registrarse </v-btn>
+        
+      </v-card>
+
+  </v-layout>
+</v-flex>
+</template>
